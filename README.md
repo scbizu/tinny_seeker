43c55ba713db670ee6f26c834e2acb0385350987
scnace <scbizu@gmail.com>
2016-08-11 16:53:02 +0800

    cli added and support single layer crawl and multi layer crawl

--- README.md
+++ README.md
@@ -7,12 +7,13 @@
 
 * 爬虫(Spider)
 
-  * 得到源站URL:站点爬虫URL数量`(-n int)`可定制，爬到的URL可以加入数据库中.做持久化存储,以便`Scheduler`后期继续获取。
+  * 得到源站URL:站点爬虫爬取的`层数`做限制`(-n int)`可定制，爬到的URL可以加入数据库中.做持久化存储,以便`Scheduler`后期继续获取。
+  * 在`Processor`中处理单条URL时 把这条URL加入Memcache/Redis中  
   * 分析URL:`Downloader`负责分析源URL请求(`POST`,`GET`...)，入库,一个URL Record 对应一个`请求类型` 和 `原始带参`
   * 拿Response:通过`Page`模块在`Processor`中获取Response，入库。
 * 扫描器(Scanner)
 
-  * 对数据库中的URL进行扫描:对**特定的**页面装载`攻击脚本`后 请求这个URL ，把`Response` Update到这个URL的Record
+  * 对Memcache/Redis中的URL进行扫描:对**特定的**页面装载`攻击脚本`后 请求这个URL ，把`Response` Update到这个URL的Record
   * 合并`Scanner`到Spider的`Downloader` 整合为一个`Seeker` 。重写`Downloader`逻辑,让`Downloader`变成一个可定制的`Scriptloader`
 
 * 设计图:
--- seeker.go
+++ seeker.go
@@ -7,8 +7,20 @@ import (
 	"github.com/fatih/color"
 	"github.com/hu17889/go_spider/core/common/page"
 	"github.com/hu17889/go_spider/core/common/request"
+	"github.com/hu17889/go_spider/core/scheduler"
 	"github.com/hu17889/go_spider/core/spider"
+	"github.com/mkideal/cli"
 )
+
+type argT struct {
+	cli.Helper
+	Layer int  `cli:"la" usage:"crawl layer ,a int type,default is 1" dft:"1"`
+	Zone  bool `cli:"!local" usage:"use this command to crawl the URL under the same domain"`
+	// stroe bool `cli:"!stroe" usage:"use this command to stroe the url "`
+}
+
+//crawl layer
+var layer int
 
 //Processor
 type SeekerProcessor struct {
@@ -21,19 +33,25 @@ func NewSeeker() *SeekerProcessor {
 
 //Process
 func (object *SeekerProcessor) Process(p *page.Page) {
-	// var urls []string
+
+	var urls []string
 	if !p.IsSucc() {
 		color.Red(p.Errormsg())
 		return
 	}
 	query := p.GetHtmlParser()
-	query.Find(".anony-nav-links ul li").Each(func(i int, s *goquery.Selection) {
-		value, _ := s.Find("a").Attr("href")
-		name := s.Find("a").Text()
+	query.Find("a").Each(func(i int, s *goquery.Selection) {
+		value, _ := s.Attr("href")
+		name := s.Text()
+		urls = append(urls, value)
 		p.AddField(name, value)
 	})
-	//continue to get links
-	// p.AddTargetRequest(urls, "html")
+
+	//add urls to scheduler
+	if layer-1 > 0 {
+		p.AddTargetRequests(urls, "html")
+		layer--
+	}
 
 }
 
@@ -42,17 +60,27 @@ func (object *SeekerProcessor) Finish() {
 }
 
 func main() {
+
 	sp := spider.NewSpider(NewSeeker(), "DoubanSeeker")
 	//Douban Root diretory
-	req := request.NewRequest("https://www.douban.com/", "html", "", "GET", "", nil, nil, nil, nil)
-	pageItems := sp.GetByRequest(req)
-	//pageItems := sp.Get("http://baike.baidu.com/view/1628025.htm?fromtitle=http&fromid=243074&type=syn", "html")
+	req := request.NewRequest("http://www.dirtytao.com/", "html", "", "GET", "", nil, nil, nil, nil)
 
-	// url := pageItems.GetRequest().GetUrl()
-	fmt.Println("-----------------------------------Result---------------------------------")
-	// fmt.Println("url\t:\t" + url)
-	for name, value := range pageItems.GetAll() {
-		color.Green(name + "\t:\t" + value + "\n")
-	}
+	//Run Client
+	cli.Run(new(argT), func(ctx *cli.Context) error {
 
+		argv := ctx.Argv().(*argT)
+		layer = argv.Layer
+		if argv.Zone {
+			//TODO:crawl the local domain
+		}
+
+		// layer = argv.Layer
+		pageItems := sp.SetThreadnum(4).SetScheduler(scheduler.NewQueueScheduler(true)).GetByRequest(req)
+		// color.Red("%d", layer)
+		fmt.Println("-----------------------------------Result---------------------------------")
+		for name, value := range pageItems.GetAll() {
+			color.Green(name + "\t:\t" + value + "\n")
+		}
+		return nil
+	}, "cli for the tinny_seeker")
 }
